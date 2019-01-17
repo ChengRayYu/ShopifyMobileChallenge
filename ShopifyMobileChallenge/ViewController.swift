a04b5f6c09e3d50f7077229294d6af67ec63c90d
Ray <ray.cj.yu@gmail.com>
2019-01-17 09:32:55 -0500

    Initial Commit

---
+++ ShopifyMobileChallenge/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ShopifyMobileChallenge
+//
+//  Created by Ray on 2019/1/17.
+//  Copyright © 2019 chengrayyu.com. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
